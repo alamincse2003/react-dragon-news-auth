ac7e3e0938d4dffa964889cc43d498e82887443b
alamincse2003 <mdalamincse2003@gmail.com>
2024-11-13 14:03:30 +0600

    firebase file modified

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCbcqSzd75tJ1IVZr6LLoWJH-QupoRwDtg",
-    authDomain: "react-dragon-news-auth-91520.firebaseapp.com",
-    projectId: "react-dragon-news-auth-91520",
-    storageBucket: "react-dragon-news-auth-91520.firebasestorage.app",
-    messagingSenderId: "945368078781",
-    appId: "1:945368078781:web:6dfa54937a65d28e385ad4"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
